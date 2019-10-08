02e547e1aa4e68cb386414b42a92dc622325429b
Oleg.Orlov <orlov.o@mail.ru>
2019-10-08 21:43:14 +0300

    Draw obstacle frames - intermediate task

--- animation/trash.py
+++ animation/trash.py
@@ -6,7 +6,12 @@ from .curses_tools import (
     upload_sprite,
     sleep,
     draw_frame,
+    get_frame_size,
 )
+from .obstacles import Obstacle, show_obstacles
+#remove then
+obstacles = []
+
 
 async def fly_trash(canvas, column, garbage_frame, speed=0.5):
     """Animate garbage, flying from top to bottom. Сolumn position will stay same, as specified on start."""
@@ -19,9 +24,11 @@ async def fly_trash(canvas, column, garbage_frame, speed=0.5):
 
     while row < rows_number:
         draw_frame(canvas, row, column, garbage_frame)
+        obstacles.append(Obstacle(row, column, *get_frame_size(garbage_frame)))
         await asyncio.sleep(0)
         draw_frame(canvas, row, column, garbage_frame, negative=True)
         row += speed
+        obstacles.pop(0)
 
 
 
@@ -30,6 +37,8 @@ async def fill_orbit_with_trash(canvas, coroutines, speed=20):
     trash_sprites = upload_sprite('trash')
     max_row, max_column = canvas.getmaxyx()
 
+    #remove then
+    coroutines.append(show_obstacles(canvas, obstacles))
     while True:
         sprite = random.choice(trash_sprites)
         start_column = random.randint(1, max_column - 1)
